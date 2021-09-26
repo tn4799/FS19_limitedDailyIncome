b0f68c0bc26041572ca831095ad72ed10cb5ff61
tn47 <tn4799@gmail.com>
2021-09-26 03:06:43 +0200

    implemented multiple parts
    implemented:
    tracking of income
    disableing mission activation if limit is fullfilled
    started disableing making money with selling animals

--- LimitedDailyIncome.lua
+++ LimitedDailyIncome.lua
@@ -1,2 +1,61 @@
 LimitedDailyIncome = {}
 
+LimitedDailyIncome.sales = {}
+LimitedDailyIncome.wasPlayerOnline = {}
+LimitedDailyIncome.salesLimit = {}
+
+LimitedDailyIncome.STANDARD_LIMIT = 500000
+LimitedDailyIncome.INCREASE_LIMIT_FACTOR = 250000
+
+-- daily reset to defaults
+function LimitedDailyIncome:onDayChanged()
+    for farmId, _ in pairs(self.sales) do
+        self.sales[farmId] = 0
+
+        if not self.wasPlayerOnline[farmId] then
+            self.salesLimit[farmId] = self.salesLimit[farmId] + self.INCREASE_LIMIT_FACTOR
+        else
+            self.salesLimit[farmId] = self.STANDARD_LIMIT
+        end
+
+        self.wasPlayerOnline[farmId] = false
+    end
+end
+
+-- keep track of earned money to measure the total amount
+function LimitedDailyIncome:addMoney(amount, farmId, moneyType, addChange, forceShowChange)
+    if amount > 0 and not moneytype == MoneyType.SHOP_VEHICLE_SELL then
+        LimitedDailyIncome.sales[farmId] = LimitedDailyIncome.sales[farmId] + amount
+    end
+end
+
+-- disable mission activation if sales limit is passed
+function LimitedDailyIncome:startMission(superFunc, mission, farmId, spawnVehicles)
+    if LimitedDailyIncome.sales[farmId] >= LimitedDailyIncome.salesLimit[farmId] then
+        --TODO: Show error "you have earned to much money today. Please wait till the next day." on screen
+        return
+    end
+
+    superFunc(mission, farmId, spawnVehicles)
+end
+
+-- disable making money with selling animals
+function LimitedDailyIncome:applyChangesTrailer(superFunc)
+    -- get vehicle the trailer is attached to
+    local vehicle = self.trailer:getAttacherVehicle()
+    local farmId = vehicle:getActiveFarm();
+    
+    if LimitedDailyIncome.sales[farmId] >= LimitedDailyIncome.salesLimit[farmId] then
+        --TODO: Show error "you have earned to much money today. Please wait till the next day." on screen
+        return
+    end
+
+    superFunc(self)
+end
+
+FSBaseMission.addMoney = Utils.prependedFunction(FSBaseMission.addMoney, LimitedDailyIncome.addMoney)
+MissionManager.startMission = Utils.overwrittenFunction(MissionManager.startMission, LimitedDailyIncome.startMission)
+
+
+--TODO
+--g_currentMission:addUpdateable()
